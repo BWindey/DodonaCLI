7df5b85516920cf7f46afb3641cb7abffa9a9ca9
Bram Windey <windey.bram@gmail.com>
2023-10-29 16:47:57 +0100

    Messing around with argparser

--- main.py
+++ main.py
@@ -1,25 +1,57 @@
 import argparse
+import http.client
+import json
 
 
 class ArgumentParser:
     def __init__(self, description):
-        self.parser = argparse.ArgumentParser(description=description)
+        self.parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
+        self.add_arguments()
         self.args = None
+        self.parse_args()
 
     def add_arguments(self):
-        # Define your command-line arguments here
-        self.parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose mode")
+        # Command-line arguments
+        self.parser.add_argument("-c", "--show_courses",
+                                 metavar='OPTION',
+                                 nargs='?',
+                                 choices=['a', 'm', 'i', 'f'],
+                                 default='m',
+                                 help="Show courses (a: all, m: my (default), i: institution, f: featured")
 
     def parse_args(self):
         self.args = self.parser.parse_args()
 
 
-def main():
-    arg_parser = ArgumentParser("Dodona Command Line Interface (dodona.be)")
-    arg_parser.add_arguments()
-    arg_parser.parse_args()
+def show_courses(connection, headers, tab='my'):
+    connection.request("GET", "/courses.json?tab=" + tab, headers=headers)
+    res = connection.getresponse()
+    print(res.status, res.reason)
+    data = res.read()
+    connection.close()
 
+    pretty_json = json.dumps(json.loads(data), indent=2)
+    print(pretty_json)
+
+
+def main():
+    token_file = open("token")
+    TOKEN = token_file.read()[:-1]
+    token_file.close()
+
+    arg_parser = ArgumentParser("Dodona Command Line Interface (dodona.be)")
     print(arg_parser.args)
+
+    connection = http.client.HTTPSConnection("dodona.be")
+    headers = {
+        "Content-type": "application/json",
+        "Accept": "application/json",
+        "Authorization": TOKEN
+    }
+
+    if arg_parser.args.show_courses:
+        print(arg_parser.args.show_courses)
+        show_courses(connection, headers,)
 
 
 if __name__ == "__main__":
