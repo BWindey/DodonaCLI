d2f918949cc913ca2b99b19c75b35218107aafc1
Bram Windey <windey.bram@gmail.com>
2023-11-03 14:10:42 +0100

    Added comments and documentation

--- get_data.py
+++ get_data.py
@@ -1,4 +1,3 @@
-import http.client
 import json
 import os
 
@@ -6,6 +5,12 @@ from set_data import dump_config
 
 
 def courses_data(connection, headers):
+    """
+    Get registred courses of user
+    :param connection: HTTPSConnection object to the main Dodona page
+    :param headers: Dict with extra info, mainly autorization needed
+    :return: json object with info about available courses
+    """
     connection.request("GET", "/courses.json?tab=my", headers=headers)
     res = connection.getresponse()
     if res.status != 200:
@@ -21,6 +26,13 @@ def courses_data(connection, headers):
 
 
 def series_data(connection, headers, course_id):
+    """
+    Get all exercise-series of course
+    :param connection: HTTPSConnection object to the main Dodona page
+    :param headers: Dict with extra info, mainly autorization needed
+    :param course_id: int id of the course to find series from
+    :return: json object with info about available series
+    """
     connection.request("GET", "/courses/" + course_id + "/series.json", headers=headers)
     res = connection.getresponse()
     if res.status != 200:
@@ -34,6 +46,13 @@ def series_data(connection, headers, course_id):
 
 
 def exercises_data(connection, headers, series_id):
+    """
+    Get all exercises of exercise-serie
+    :param connection: HTTPSConnection object to the main Dodona page
+    :param headers: Dict with extra info, mainly autorization needed
+    :param series_id: int id of exercise-series
+    :return: json object with info about available exercises
+    """
     connection.request("GET", "/series/" + series_id + "/activities.json", headers=headers)
     res = connection.getresponse()
     if res.status != 200:
@@ -47,6 +66,14 @@ def exercises_data(connection, headers, series_id):
 
 
 def exercise_data(connection, headers, course_id, exercise_id):
+    """
+    Get exercise-info for selected exercise
+    :param connection: HTTPSConnection object to the main Dodona page
+    :param headers: Dict with extra info, mainly autorization needed
+    :param course_id: int id of course
+    :param exercise_id: int id of exercise
+    :return: json object with info about exercise
+    """
     connection.request("GET", "/courses/" + course_id + "/activities/" + exercise_id + ".json", headers=headers)
     res = connection.getresponse()
     if res.status != 200:
@@ -61,9 +88,17 @@ def exercise_data(connection, headers, course_id, exercise_id):
 
 
 def get_configs():
+    """
+    Get config data from config.json
+    :return: json object with config data
+    """
+    # Get path of the config-file. This is a bit more complicated because this file exists at the same directory as
+    # the python files, but the command may be executed from anywhere with the apropriate alias set.
+    # Thus, first te path to the directory of the python files is retrieved, then the config-file-name is appended
     script_directory = os.path.dirname(os.path.abspath(__file__))
     config_file_path = os.path.join(script_directory, 'config.json')
 
+    # First try to open, if unable to open, create new config-file and ask user for a token
     try:
         with open(config_file_path, "r") as file:
             config = json.load(file)
@@ -74,6 +109,7 @@ def get_configs():
         config["course_id"] = None
         config["serie_id"] = None
         config["exercise_id"] = None
+        # Save configs
         dump_config(config)
 
     return config
--- pretty_print.py
+++ pretty_print.py
@@ -3,31 +3,49 @@ import shutil
 import re
 import textwrap
 
-from pretty_console import console, escape
+from pretty_console import console
 
 
 def print_courses_data(json_data):
+    """
+    Print out the courses in json_data in a neat way
+    :param json_data: json object with data about Dodona courses
+    """
+    # List of tuples where each tuple represents a course by id, name and teacher
     display_data = []
 
     for field in json_data:
         display_data.append((str(field['id']), field['name'], field['teacher']))
+
+    # Find the maximum length of all but the last element in all tuples to align them in the terminal
     max_course_id_length = max(len(e[0]) for e in display_data)
     max_course_name_length = max(len(e[1]) for e in display_data)
+
+    # Sort the courses by name
     display_data = sorted(display_data, key=lambda x: x[1])
 
+    # Print out all courses in display_data
     console.print('[u bright_blue]Your courses:[/]')
     for e in display_data:
         print(f'{e[0].ljust(max_course_id_length)}: \033[1m{e[1].ljust(max_course_name_length)}\033[0m\tby {e[2]}')
 
 
 def print_series_data(json_data):
+    """
+    Print out the exercise-series in json_data in a neat way
+    :param json_data: json object with data about Dodona exercise-series
+    """
+    # List of tuples where each tuple represents an exercise-series by id, name and description
     display_data = []
 
     for field in json_data:
         display_data.append((str(field['id']), field['name'], field['description']))
+
+    # Find the maximum length of all but the last element in all tuples to align them in the terminal
     max_series_id_length = max(len(e[0]) for e in display_data)
     max_series_name_length = max(len(e[1]) for e in display_data)
 
+    # Print out all the series in display_data while also handling the Markdown inside the series-description
     console.print("[u bright_blue]All series:[/]")
     for e in display_data:
         description = e[2].split('\n')
@@ -42,6 +60,7 @@ def print_series_data(json_data):
             line = re.sub(r'\*\*(.*?)\*\*', '\033[1m\\1\033[0m', line)
             line = re.sub(r'_(.*?)_', '\033[3m\\1\033[0m', line)
 
+            # Split lines in multiple when they are too long for the terminal while keeping all lines indented.
             if len(line.replace("**", "").replace("_", "")) > shutil.get_terminal_size().columns - 8:
                 line = line.split(" ")
                 new_line = ''
@@ -62,17 +81,25 @@ def print_series_data(json_data):
 
 
 def print_exercise_data(json_data):
+    """
+    Print out the exercises in json_data in a neat way
+    :param json_data: json object with data about Dodona exercises in a series
+    """
+    # List of tuples where each tuple represents an exercise by id, name, solved and has_attempt
     display_data = []
 
     for field in json_data:
         display_data.append((str(field['id']), field['name'], field['last_solution_is_best'], field['has_solution']))
+
+    # Find the maximum length of all but the last element in all tuples to align them in the terminal
     max_exercise_id_length = max(len(e[0]) for e in display_data)
     max_exercise_name_length = max(len(e[1]) for e in display_data)
 
+    # Print out all exercises in display_data with indicator about solution-status: solved, wrong or not yet solved
     console.print('[u bright_blue]Exercises:[/]')
     for e in display_data:
         console.print(
-            f"{escape(e[0].ljust(max_exercise_id_length))}: [bold]{e[1].ljust(max_exercise_name_length)}[/]\t" +
+            f"{e[0].ljust(max_exercise_id_length)}: [bold]{e[1].ljust(max_exercise_name_length)}[/]\t" +
             "[bold bright_green]SOLVED[/]" * (e[2] and e[3]) +
             "[bold bright_red]WRONG[/]" * (not e[2] and e[3]) +
             "[bold]NOT YET SOLVED[/]" * (not e[3])
@@ -80,15 +107,25 @@ def print_exercise_data(json_data):
 
 
 def print_exercise(json_data, connection, headers):
+    """
+    Print out the exercise-description. Needs to call the Dodona-sandbox and convert HTML to text.
+    Prints out a warning for potential incompleteness, which may be dangerous for tests and exams.
+    :param json_data: json object with info about a Dodona exercise
+    :param connection: HTTPSConnection object that connects to sandbox.Dodona.be
+    :param headers: dict with extra info for connection, mainly authorization needed
+    """
+    # Strip https://www.sandbox.dodona.be from the description_url and get the description in HTML
     description_url = json_data['description_url'][28:]
     connection.request("GET", description_url, headers=headers)
     res = connection.getresponse()
     data = res.read()
     connection.close()
 
+    # Parse the HTML with BeautifulSoup and take onle the description
     soup = BeautifulSoup(data, features="html.parser")
     description = soup.find("div", {"class": "activity-description"})
 
+    # Print the HTML with warnings
     print("\033[1;4;91mWARNING: the description may not be correct, DO NOT rely on this for exams and tests!!\n"
           "Instead, use this url:\033[0m " + json_data['description_url'])
     print(description.get_text())
@@ -97,9 +134,15 @@ def print_exercise(json_data, connection, headers):
 
 
 def print_result(json_results):
+    """
+    Print out the results of a submission in a neat way
+    :param json_results: json object with data about a submission
+    """
     if json_results['accepted']:
+        # Everything passed, well done!
         console.print("[bold bright_green]All test passed![/] You can continue to next exercise.")
     else:
+        # There were some problems, list them here
         for group in json_results['groups']:
             print(group['description'] + ": " + str(group['badgeCount']) + " tests failed.")
 
