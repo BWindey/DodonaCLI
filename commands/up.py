89f5b580268772143faf47fa8f496fa9ec80d62a
Bram Windey <windey.bram@gmail.com>
2023-11-13 17:20:20 +0100

    Moved logic for up from handle_flags.py to up.py

--- commands/up.py
+++ commands/up.py
@@ -1,18 +1,42 @@
 import click
 
-from source import handle_flags, get_data
+from source import get_data, set_data
 
 
 @click.command(help="Deselect something")
-@click.argument('amount', default='1')
+@click.argument('amount', default='1',
+                type=click.Choice(['all', 'top', '1', '2', '3'], case_sensitive=False))
 def up(amount):
     # Read configs in
     config = get_data.get_configs()
 
-    if amount.strip() == '1':
-        handle_flags.handle_up(config)
-    elif amount.strip() == '2':
-        handle_flags.handle_up(config)
-        handle_flags.handle_up(config)
+    if str(amount).lower().strip() in ('all', 'top'):
+        for e in ('exercise_id', 'exercise_name', 'serie_id', 'serie_name', 'course_id', 'course_name'):
+            config[e] = None
+        print("\nDeselected everything.\n")
+
+    elif not isinstance(amount, int) and not amount.isnumeric():
+        print("You didn't provide a number or 'all', nothing deselected.")
+
     else:
-        handle_flags.handle_uptop(config)
+        levels = int(amount)
+        print()
+        for _ in range(levels):
+            if config.get('exercise_id') is not None:
+                config['exercise_id'] = None
+                config['exercise_name'] = None
+                print("Deselected exercise.")
+            elif config.get('serie_id') is not None:
+                config['serie_id'] = None
+                config['serie_name'] = None
+                print("Deselected series.")
+            elif config.get('course_id') is not None:
+                config['course_id'] = None
+                config['course_name'] = None
+                print("Deselected course")
+            else:
+                print("Nothing selected.")
+                break
+        print()
+
+    set_data.dump_config(config)
