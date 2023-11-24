abd34740e7a839256c2ab4df557aba424dd4a5ab
Bram Windey <windey.bram@gmail.com>
2023-11-24 16:38:12 +0100

    Updated install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 if [[ $SHELL =~ "bash" ]]; then
 	pip install -r requirements.txt
 	chmod u+x main.py
-	echo "alias dodona=$(realpath main.py)" >> ~/.bashrc
+	echo "alias dodona=python3 $(realpath main.py) $*" >> ~/.bashrc
 	echo "source $(realpath completion_script.sh)" >> ~/.bashrc
-	source ~/.bashrc
+	source "${HOME}/.bashrc"
 else
 	echo "Script only works for bash for now, see the README.md for other platforms"
 fi
