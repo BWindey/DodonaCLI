656844e1a2af7046c366e81e3dfac89be0c1defb
Bram Windey <windey.bram@gmail.com>
2023-11-24 16:02:58 +0100

    Added pip install to install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 if [[ $SHELL =~ "bash" ]]; then
 	echo "source $(realpath completion_script.sh)" >> ~/.bashrc
 	source completion_script.sh
+	pip install -r requirements.txt
 else
 	echo "not bash =("
 fi
