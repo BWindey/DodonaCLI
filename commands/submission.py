141cbbe310f15fbe3fdab49ed5446be35f2283df
Bram Windey <windey.bram@gmail.com>
2023-11-28 19:48:58 +0100

    Added click_default_group and put imports in submission.py inside a try-except block

--- commands/submission.py
+++ commands/submission.py
@@ -1,14 +1,44 @@
-import click
-import http.client
+import os
+
+try:
+    import click
+    import http.client
+
+    from click_default_group import DefaultGroup
+
+except ImportError as e:
+    answer = input("There were unmet dependencies: \n"
+                   + str(e)
+                   + '\nDo you want to install them automatically? [y/n]: ')
+    if not answer.lower().startswith('y'):
+        exit("Exited due to unmet dependencies")
+    os.system('python -m pip install -r' + os.path.dirname(__file__)[:-8] + 'requirements.txt')
+
+    import click
+    import http.client
+
+    from click_default_group import DefaultGroup
+
 
 from source import get_data, pretty_print
 
 
-@click.command(help="Display up to your 10 lasts submissions for the current selected exercise.")
+@click.group(help="Get submission data. Default = view",
+             cls=DefaultGroup, default='view', default_if_no_args=True)
+def sub():
+    pass
+
+
+@click.command(help="Load submission data")
 @click.option("-l", "--load", type=int, default=-1,
               help="NOT WORKY YET Load a submission to the prev_submission file, and display more info about it. "
                    "You can specify a number, else it takes the last submission for that exercise.")
-def sub(load):
+def load():
+    pass
+
+
+@click.command(help="View subimmision data")
+def view():
     # Read configs in
     config = get_data.get_configs()
 
@@ -27,3 +57,7 @@ def sub(load):
 
         else:
             print("Nothing to display yet.")
+
+
+sub.add_command(load)
+sub.add_command(view)
