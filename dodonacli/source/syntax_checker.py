74c02da442442356ebd0fc53beca08ceb155a88a
Bram Windey <windey.bram@gmail.com>
2024-01-11 14:49:24 +0100

    Implemented Python syntax checker

--- dodonacli/source/syntax_checker.py
+++ dodonacli/source/syntax_checker.py
@@ -4,6 +4,8 @@ import subprocess
 def check_syntax(file: str, language: str) -> bool:
     if language == "bash":
         return check_bash_syntax(file)
+    if language == "python":
+        return check_python_syntax(file)
     return True
 
 
@@ -20,6 +22,18 @@ def check_bash_syntax(file: str) -> bool:
         print("\nTo check the syntax, 'shellcheck' is called with your file. It appears however, that "
               "shellcheck isn't installed on your system. Please install it: https://www.shellcheck.net/")
         return False
+    except Exception as e:
+        print("No idea what's going wrong, but something definitly is going wrong:\n" + str(e))
+        return False
+
+
+def check_python_syntax(file: str) -> bool:
+    try:
+        with open(file, 'r') as content:
+            compile(content.read(), file, 'exec')
+        return True
+    except SyntaxError as se:
+        print(f"\nSyntax error in Python file:\n{se}")
     except Exception as e:
         print("No idea what's going wrong, but something definitly is going wrong:\n" + str(e))
         return False
