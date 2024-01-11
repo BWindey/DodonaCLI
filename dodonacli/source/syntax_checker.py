b244b83488dc607ccf851e8f500faf8016eacc91
Bram Windey <windey.bram@gmail.com>
2024-01-11 19:45:41 +0100

    Implemented Java and JavaScript syntax checker

--- dodonacli/source/syntax_checker.py
+++ dodonacli/source/syntax_checker.py
@@ -1,11 +1,18 @@
 import subprocess
+import tempfile
+import shutil
 
 
 def check_syntax(file: str, language: str) -> bool:
-    if language == "bash":
+    if language in ("bash", "sh"):
         return check_bash_syntax(file)
     if language == "python":
         return check_python_syntax(file)
+    if language == "java":
+        return check_java_syntax(file)
+    if language == "javascript":
+        return check_javascript_syntax(file)
+    print("\nLanguage syntax check is not implemented for: " + language)
     return True
 
 
@@ -23,7 +30,7 @@ def check_bash_syntax(file: str) -> bool:
               "shellcheck isn't installed on your system. Please install it: https://www.shellcheck.net/")
         return False
     except Exception as e:
-        print("No idea what's going wrong, but something definitly is going wrong:\n" + str(e))
+        print("\nNo idea what's going wrong, but something definitly is going wrong:\n" + str(e))
         return False
 
 
@@ -35,5 +42,38 @@ def check_python_syntax(file: str) -> bool:
     except SyntaxError as se:
         print(f"\nSyntax error in Python file:\n{se}")
     except Exception as e:
-        print("No idea what's going wrong, but something definitly is going wrong:\n" + str(e))
+        print("\nNo idea what's going wrong, but something definitly is going wrong:\n" + str(e))
+        return False
+
+
+def check_java_syntax(file: str) -> bool:
+    temp_dir = tempfile.mkdtemp()
+    try:
+        subprocess.run(['javac', '-d', temp_dir, file], check=True)
+        shutil.rmtree(temp_dir, ignore_errors=True)
+        return True
+
+    except FileNotFoundError:
+        # This will only occur if javac isn't installed.
+        # Click will detect that the user gave an invalid file before this function is called
+        print("\nTo check the syntax, 'javac' is called with your file. It appears however, that "
+              "the Java compiler isn't installed on your system. Please install it: https://openjdk.org/install/")
+        return False
+    except Exception as e:
+        print("\nNo idea what's going wrong, but something definitly is going wrong:\n" + str(e))
+        return False
+    finally:
+        shutil.rmtree(temp_dir, ignore_errors=True)
+
+
+def check_javascript_syntax(file: str) -> bool:
+    try:
+        subprocess.run(['jshint', file], check=True)
+        return True
+    except FileNotFoundError:
+        print("\nTo check the syntax, 'jshint' is called with your file. It appears however, that "
+              "this program isn't installed on your system. Please install it using npm: npm install -g jshint")
+        return False
+    except Exception as e:
+        print("\nNo idea what's going wrong, but something definitly is going wrong:\n" + str(e))
         return False
